197db917aa4f22557cd0367c0a372c6a0416570d
Palma <1hernandezj3@gmail.com>
2021-11-10 18:41:16 -0500

    Created an NPM library and add files to it that would be common to my project

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@palmpass/common';
 import { User } from '../models/users';
-import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
